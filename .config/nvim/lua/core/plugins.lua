575e616bf7c2ac59ca267c9c447d37f75a5c1f26
cjmckernan <conorjmckernan@gmail.com>
2024-06-17 00:46:08 +0100

    autoformatting for rust files.

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,8 +1,8 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -25,8 +25,12 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'kylechui/nvim-surround'
   use 'windwp/nvim-autopairs'
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    require = { 'nvim-lua/plenary.nvim' }
+  }
 
-  -- Auto complete 
+  -- Auto complete
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
